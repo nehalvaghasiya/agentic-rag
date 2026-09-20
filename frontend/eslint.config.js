d306bdd049bf5b5c0f0c83781bdf3017987f9fe7
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2026-09-19 22:46:24 -0500

    update eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,53 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import { reactRefresh } from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+
+const languageOptions = {
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+  globals: globals.browser,
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+  },
+}
+
+const reactPlugins = {
+  'react-hooks': reactHooks,
+  'react-refresh': reactRefresh.plugin,
+}
+
+const reactRules = {
+  'react-hooks/rules-of-hooks': 'error',
+  'react-hooks/exhaustive-deps': 'error',
+  'react-refresh/only-export-components': [
+    'error',
+    {
+      allowConstantExport: true,
+      allowExportNames: ['useAppState'],
+    },
+  ],
+}
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
+    extends: [js.configs.recommended],
+    languageOptions,
+    plugins: reactPlugins,
     rules: {
+      ...reactRules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions,
+    plugins: reactPlugins,
+    rules: reactRules,
   },
 ])
